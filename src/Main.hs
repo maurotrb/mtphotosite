0a3c884ee0f3aaa2e801bdd4bb1d7349e8d82f4d
Mauro Taraborelli <mauro@maurotaraborelli.com>
2014-02-03 16:14:25 +0100

    Setup cabalized project with cabal.config to freeze libraries version.

---
+++ src/Main.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Main
+-- Copyright   :  Mauro Taraborelli 2014
+-- License     :  BSD3
+--
+-- Maintainer  :  mauro@maurotaraborelli.com
+--
+-- Photography portfolio site made with Hakyll
+
+module Main
+    (
+     main
+    )
+where
+
+main :: IO ()
+main = return ()
+
